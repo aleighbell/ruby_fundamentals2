e5ef07374391db8bce469ce19685625ccff634a1
aleighbell <aleighbell@gmail.com>
2016-08-16 13:40:30 -0400

    Add files via upload

---
+++ exercise5.rb
@@ -0,0 +1,13 @@
+
+    puts "Hello! What's your name?"
+      name = gets
+        puts "Nice to meet you, #{name}"
+
+    puts "Please enter temperature in F"
+        F = gets
+
+          def method_temperature
+             ((F - 32) * 5/9).each
+             return new_temperature
+             
+          end
