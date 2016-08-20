5daa8ab7e98f3cfd2bc3a92bf47ab1f5e18fa67b
aleighbell <aleighbell@gmail.com>
2016-08-20 07:21:33 -0400

    Add files via upload

--- exercise5.rb
+++ exercise5.rb
@@ -1,12 +1,13 @@
+ puts "Hello! What's your name?"
+   name = gets
+  puts "Nice to meet you, #{name}"
 
-    puts "Hello! What's your name?"
-      name = gets
-        puts "Nice to meet you, #{name}"
+  puts "Please enter temperature in F"
+    f = gets
 
-    puts "Please enter temperature in F"
-        F = gets
-
-   def new_temperature
-     puts "Here is your new temperature #{(F - 32) * 5/9}"
-   end
+  def new_temperature
+   puts "Here is your new temperature"
+  end
+    puts "{(f - 32) * 5/9}"
+  end
 end
