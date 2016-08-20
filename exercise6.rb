e6fe401f6597adf6ad55e9ceba955c70fb73a9e7
aleighbell <aleighbell@gmail.com>
2016-08-20 07:41:29 -0400

    figuring out the correct method for length

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,9 @@
-  grocery_list = ["carrots", "apples", "oranges", "potatoes", "lemons"]
+Grocery_list = ["carrots", "apples", "oranges", "potatoes", "lemons"]
+        *carrots
+        *apples
+        *oranges
+        *potatoes
+        *lemons
+Grocery_list << "cat food"
 
-       *carrots
-       *apples
-       *oranges
-       *potatoes
-       *lemons
-  grocery_list = "cat food" <<
-  
+Grocery_list.length
