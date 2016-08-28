768aec708e6082b7c72833c006fa11dad27aea97
aleighbell <aleighbell@gmail.com>
2016-08-28 09:07:51 -0400

    increased cohort size and deleted cohort 2

--- exercise7.rb
+++ exercise7.rb
@@ -16,3 +16,15 @@ students = {
 student_count(students)
 
 students[:cohort4] = "43"
+
+puts students.keys
+
+#each class has grown by 5%
+
+students.each do |coh, num|
+  puts "#{coh}: #{num * 1.05}"
+end
+
+students.delete(:cohort2)
+
+show_hash(students)
