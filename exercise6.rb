294f77fdfaba2c51ec3307c362b20ebf29a9422d
aleighbell <aleighbell@gmail.com>
2016-08-28 08:37:56 -0400

    corrected grocery_list assignment

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,25 @@
-Grocery_list = ["carrots", "apples", "oranges", "potatoes", "lemons"]
-        *carrots
-        *apples
-        *oranges
-        *potatoes
-        *lemons
-Grocery_list << "cat food"
+grocery_list = ["carrots", "lemon", "celery","cucumber", "apples"]
 
-Grocery_list.length
+def list_groceries
+  grocery.each do |item| puts "* #{item}"
+end
+
+grocery_list << "tomatoes"
+
+puts grocery_list.length
+
+if grocery_list.include?("bananas")
+  puts "You need bananas!"
+else
+  puts "You don't need dem bananas today"
+end
+
+puts "as requested, the second item in your list #{grocery_list[1]}"
+
+print list_groceries(grocery_list.sort)
+
+puts "seems they're out of apples today"
+
+grocery_list.pop
+
+puts list_groceries
